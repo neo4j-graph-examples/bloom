a4b089b5610a1cfad0e5634ff897fd29a4a7b463
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:12:18 +0000

    Updating sandbox bloom

---
+++ code/javascript/example.js
@@ -0,0 +1,28 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://<HOST>:<BOLTPORT>',
+                  neo4j.auth.basic('<USERNAME>', '<PASSWORD>'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (i:IP {ip:$ipAddress})-[r:FROM_IP]-(p:Purchase)
+  RETURN p.amount as amount
+  `;
+
+const params = {"ipAddress": "168.166.144.243"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query, params)
+  .then((result) => {
+    result.records.forEach((record) => {
+        console.log(record.get('amount'));
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (i:IP {ip:$ipAddress})-[r:FROM_IP]-(p:Purchase)
+RETURN p.amount as amount
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      ipAddress="168.166.144.243").data())
+
+  for record in results:
+    print(record['amount'])
+
+driver.close()
