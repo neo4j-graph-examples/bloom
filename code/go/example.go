be5b5127493b6307cbebbadccc8584c6db800282
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-12-14 15:04:43 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/06cf0cc68341e050e3b12a5a7889bb3fde287eba

--- code/go/example.go
+++ code/go/example.go
@@ -3,9 +3,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
-	"io"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"reflect"
 )
 
@@ -19,46 +19,37 @@ func main() {
 	}
 }
 
-func runQuery(uri, database, username, password string) (result []string, err error) {
-	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""))
+func runQuery(uri, database, username, password string) (_ []string, err error) {
+	ctx := context.Background()
+	driver, err := neo4j.NewDriverWithContext(uri, neo4j.BasicAuth(username, password, ""))
 	if err != nil {
 		return nil, err
 	}
-	defer func() {err = handleClose(driver, err)}()
-	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead, DatabaseName: database})
-	defer func() {err = handleClose(session, err)}()
-	results, err := session.ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
-		result, err := transaction.Run(
-			`
-			MATCH (i:IP {ip:$ipAddress})-[r:FROM_IP]-(p:Purchase)
-			RETURN p.amount as amount
-			`, map[string]interface{}{
-				"ipAddress": "168.166.144.243",
-			})
+	defer func() { err = handleClose(ctx, driver, err) }()
+	query := "	MATCH (i:IP {ip:$ipAddress})-[r:FROM_IP]-(p:Purchase)
+	RETURN p.amount as amount
+	params := map[string]any{"ipAddress": "168.166.144.243"}
+	result, err := neo4j.ExecuteQuery(ctx, driver, query, params,
+		neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase(database),
+		neo4j.ExecuteQueryWithReadersRouting())
+	if err != nil {
+		return nil, err
+	}
+	amounts := make([]string, len(result.Records))
+	for i, record := range result.Records {
+		// this assumes all actors have names, hence ignoring the 2nd returned value
+		name, _, err := neo4j.GetRecordValue[string](record, "amount")
 		if err != nil {
 			return nil, err
 		}
-		var arr []string
-		for result.Next() {
-			value, found := result.Record().Get("amount")
-			if found {
-				arr = append(arr, value.(string))
-			}
-		}
-		if err = result.Err(); err != nil {
-			return nil, err
-		}
-		return arr, nil
-	})
-	if err != nil {
-		return nil, err
+		amounts[i] = name
 	}
-	result = results.([]string)
-	return result, err
+	return amounts, nil
 }
 
-func handleClose(closer io.Closer, previousError error) error {
-	err := closer.Close()
+func handleClose(ctx context.Context, closer interface{ Close(context.Context) error }, previousError error) error {
+	err := closer.Close(ctx)
 	if err == nil {
 		return previousError
 	}
